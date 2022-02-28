49f838e7485a434091b7666625033d3198eeab3f
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-02-28 08:53:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Adam";
+  sender = "Erik";
 }
 
 let footer = document.getElementById("credit");
 footer.innerHTML = sender;
-footer.href = "https://www.instagram.com/adamukti/";
+footer.href = "https://www.instagram.com/rickganzzxploit.id/";
 
 document.querySelector(".tombol").addEventListener('click', function () {
   Swal.fire("Hallo Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
